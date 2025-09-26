d9ea28d43c2355e407ab96529200c5da0404b519
DevolRaman750 <ramantripathi0707@gmail.com>
2025-09-26 23:31:13 +0530

    final touch 2

--- src/components/models/tech_logos/TechIconCardExperience.jsx
+++ src/components/models/tech_logos/TechIconCardExperience.jsx
@@ -3,7 +3,7 @@ import {Environment, Float, OrbitControls, useGLTF} from "@react-three/drei";
 import {Canvas} from "@react-three/fiber";
 import * as THREE from "three";
 
-const TechIcons = ({model}) => {
+const TechIconCardExperience = ({model}) => {
     const scene = useGLTF(model.modelPath);
 
     useEffect(() => {
@@ -16,7 +16,7 @@ const TechIcons = ({model}) => {
                 }
             });
         }
-    }, [scene]);
+    } );
 
     return (
         <Canvas>
@@ -30,23 +30,7 @@ const TechIcons = ({model}) => {
             />
             <Environment preset="city" />
 
-            {/*
-        The Float component from @react-three/drei is used to
-        create a simple animation of the model floating in space.
-        The rotationIntensity and floatIntensity props control the
-        speed of the rotation and float animations respectively.
 
-        The group component is used to scale and rotate the model.
-        The rotation is set to the value of the model.rotation property,
-        which is an array of three values representing the rotation in
-        degrees around the x, y and z axes respectively.
-
-        The primitive component is used to render the 3D model.
-        The object prop is set to the scene object returned by the
-        useGLTF hook, which is an instance of THREE.Group. The
-        THREE.Group object contains all the objects (meshes, lights, etc)
-        that make up the 3D model.
-      */}
             <Float speed={5.5} rotationIntensity={0.5} floatIntensity={0.9}>
                 <group scale={model.scale} rotation={model.rotation}>
                     <primitive object={scene.scene} />
@@ -58,4 +42,4 @@ const TechIcons = ({model}) => {
     );
 };
 
-export default TechIcons;
+export default TechIconCardExperience;
--- src/sections/TechStack.jsx
+++ src/sections/TechStack.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import gsap from "gsap";
 import TitleHeader from "../components/TitleHeader.jsx";
 import {techStackIcons} from "../constants/index.js";
-import TechIcons from "../components/Models/TechLogos/TechIcons.jsx";
+import TechIconCardExperience from "../components/models/tech_logos/TechIconCardExperience.jsx";
 import {useGSAP} from "@gsap/react";
 
 const TechStack = () => {
@@ -51,7 +51,7 @@ const TechStack = () => {
                                 {/* The tech-icon-wrapper div contains the TechIconCardExperience component,
                     which renders the 3D model of the tech stack icon. */}
                                 <div className="tech-icon-wrapper">
-                                    <TechIcons model={techStackIcon} />
+                                    <TechIconCardExperience model={techStackIcon} />
                                 </div>
                                 {/* The padding-x and w-full classes are used to add horizontal padding to the
                     text and make it take up the full width of the component. */}
